98881b271c808da5b037745ffd2e0b4d880e90f6
lohztmz <92582049+lohztmz@users.noreply.github.com>
2023-11-27 11:21:39 +0000

    Update projeto1.cpp
    funiconavel

--- projeto1.cpp
+++ projeto1.cpp
@@ -27,7 +27,8 @@ int calculateprice(int x, int y, std::list<std::vector<int>> pieces){
     for(double i=1; i<=x; i++){
         for(double j=i;j<=y;j++){
             //ver as peças e ver se alguma encaixa na tabela e apaga-a
-            int cur_max=0;
+            int val= 0, cur_max=0;
+
             if(!pieces.empty()){
                 for(std::list<std::vector<int>>::iterator it= pieces.begin();it !=pieces.end();){
 
@@ -46,18 +47,27 @@ int calculateprice(int x, int y, std::list<std::vector<int>> pieces){
             
             //ver combinaçoes das peças
             //iterar desde o fim até o ceilling da metade, dividindo a placa verticalmente
-            for(double a=i-1; a>=ceil(i/2); a--){
-                if(int val=subpiece_max[a][j]+subpiece_max[i-a][j] > cur_max){
+            for(int a=i-1; ceil(i/2)<=a; a--){
+                val=subpiece_max[a][j] + subpiece_max[i-a][j];
+                if(cur_max<val)
                     cur_max= val;
-                }
             } 
 
             if(i!=j) {//se não for quadrado
-                //fazer o mesmo que há pouco mas agore horizontalmente
-                for(double a=j-1; a>=ceil(j/2); a--){
-                    if(int val=subpiece_max[i][a]+subpiece_max[i][j-a] > cur_max){ 
-                        cur_max= val;
+                //fazer o mesmo que há pouco mas agora horizontalmente
+                for(int a=j-1; ceil(j/2)<=a; a--){
+                    if(j-a<=i){//verificar se o primeiro valor é menor que o segundo
+                        val = subpiece_max[j-a][i];
+                    }else{
+                        val = subpiece_max[i][j-a];
                     }
+                    if(a<=i){
+                        val += subpiece_max[a][i];
+                    }else{
+                        val += subpiece_max[i][a];
+                    }
+                    if(cur_max < val)
+                        cur_max= val;
                 } 
             }
             subpiece_max[i][j]= cur_max;   
